ef0d2ee196e2df7f2c764d0d6d33ff898b7c08bf
tianchao <tianchao@gbase.cn>
2025-02-12 18:05:52 +0800

    CONSTRAINT_TYPE : ADD UNIQUE_KEY/CHECK

---
+++ plugins/org.jkiss.dbeaver.ext.gbase8s/src/org/jkiss/dbeaver/ext/gbase8s/GBase8sConstants.java
@@ -0,0 +1,35 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2024 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jkiss.dbeaver.ext.gbase8s;
+
+/**
+ * GBase8sConstants
+ */
+public class GBase8sConstants {
+
+    //////////////////////////////////////////////////////
+    // Constraints
+
+    public static final String CONSTRAINT_TYPE = "CONSTRAINT_TYPE";
+    public static final String CONSTRAINT_TYPE_PRIMARY_KEY = "P";
+    public static final String CONSTRAINT_TYPE_UNIQUE_KEY = "U";
+    public static final String CONSTRAINT_TYPE_CHECK = "C";
+
+    public static final String CHECK_CLAUSE = "CHECK_TEXT";
+
+}
